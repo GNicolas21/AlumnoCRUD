cb911370fbeb66604179845d503f099a407e3780
GNicolas21 <giorgionicolasosoriobautista@gmail.com>
2025-12-30 22:41:30 +0100

    Anotaciones OpenAPI a las clases de alumno para mejorar la documentación de la API

--- src/main/java/es/nicolas/alumnos/dto/AlumnoUpdateDto.java
+++ src/main/java/es/nicolas/alumnos/dto/AlumnoUpdateDto.java
@@ -1,5 +1,6 @@
 package es.nicolas.alumnos.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -8,13 +9,18 @@ import lombok.Data;
 @Builder
 @Data
 @AllArgsConstructor
+@Schema(description = "Alumno a actualizar")
 public class AlumnoUpdateDto {
+    @Schema(description = "Nombre del alumno", example = "Juan")
     @Pattern(regexp = "^[A-Za-zÁÉÍÓÚáéíóúÑñÜü\\s]{1,30}$", message = "El nombre solo puede contener letras y espacios, y debe tener entre 1 y 30 caracteres.")
     private final String nombre;
     // Aquí le quito la anotación @NotBlank para poder dejar en null este campo (para probar el update())
+    @Schema(description = "Apellido del alumno", example = "Pérez")
     private final String apellido;
+    @Schema(description = "Grado del alumno", example = "1 DAW")
     @Pattern(regexp = "^[A-Za-zÁÉÍÓÚáéíóúÑñÜü0-9\\s]{1,40}$", message = "El grado solo puede contener letras, numeros y espacios, y debe tener entre 1 y 40 caracteres.")
     private final String grado;
-    //Aqui no ponemos la asignatura porque no la cambiaramos
+    //Aqui no ponemos la asignatura porque no la cambiaramos ?D
+    @Schema(description = "Asignatura del alumno", example = "Matemáticas")
     private final String asignatura;
 }
--- src/main/java/es/nicolas/alumnos/models/Alumno.java
+++ src/main/java/es/nicolas/alumnos/models/Alumno.java
@@ -1,6 +1,7 @@
 package es.nicolas.alumnos.models;
 
 import es.nicolas.asignaturas.models.Asignatura;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -17,8 +18,10 @@ import java.util.UUID;
 // Y no es recomendable usar @Data
 @Entity
 // Si no se especifica, el nombre de la tabla será el mismo que el de la clase
+@Schema(name = "Alumnos")
 @Table(name="ALUMNOS")
 public class Alumno {
+    @Schema(description = "ID del alumno", example = "1")
     // Indica que este campo es la clave primaria de la entidad
     @Id
     // Establece la estrategia de generación de valores para la clave primaria
@@ -26,26 +29,34 @@ public class Alumno {
     private Long id;
     // Por defecto, los campos son obligatorios (nullable = true)
     @Column(nullable = false, length = 30)
+    @Schema(description = "Nombre del alumno", example = "Juan")
     private String nombre;
     @Column(nullable = false, length = 30)
+    @Schema(description = "Apellido del alumno", example = "Pérez")
     private String apellido;
     @Column(nullable = false, length = 10)
+    @Schema(description = "Grado del alumno", example = "1 DAW")
     private String grado;
 
+    @Schema(description = "Fecha de creación del alumno", example = "2024-01-01T12:00:00")
     @Column(updatable = false, nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private LocalDateTime createdAt = LocalDateTime.now();
+    @Schema(description = "Fecha de última actualización del alumno", example = "2024-01-02T12:12:12")
     @Column(nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private LocalDateTime updatedAt = LocalDateTime.now();
+    @Schema(description = "UUID del alumno", example = "123e4567-e89b-12d3-a456-426614174000")
     @Column(unique = true, updatable = false, nullable = false)
     private UUID uuid = UUID.randomUUID();
 
     // Nueva columna
+    @Schema(description = "Indica si el alumno está eliminado", example = "false")
     @Column(columnDefinition = "BOOLEAN DEFAULT false")
     @Builder.Default
     private boolean isDeleted = false;
 
     @ManyToOne
     @JoinColumn(name="asignatura_id")
+    @Schema(description = "Asignatura del alumno", example = "Matemáticas")
     private Asignatura asignatura;
 
 }
